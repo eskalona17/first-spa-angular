c40597af3fb475cf2233189e855a48d9ebcdbfde
eskalona17 <eskalona_17@hotmail.com>
2020-09-20 17:32:00 +0200

    showing the details for each hero

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../servicios/heroes.service';
 
 @Component({
   selector: 'app-heroe',
-  templateUrl: './heroe.component.html'
+  templateUrl: './heroe.component.html',
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent {
+  heroe: any = {};
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private _heroesService: HeroesService
+  ) {
+    this.activatedRoute.params.subscribe((params) => {
+      this.heroe = this._heroesService.getHeroe(params['id']);
+    });
   }
-
 }
